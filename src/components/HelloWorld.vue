a6022f933ccecbbd475db36bcfa731f7f6575bd0
Raygud <runigudmundarson@hotmail.com>
2023-01-08 02:38:13 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-for="item in items" :key="item.id">
+    <img :src="item.images[0]" alt="Product name">
+    <h2 class="product-name">{{ item.title }}</h2>
+    <p class="product-description">{{ item.description }}</p>
+    <p class="product-price">{{ item.images[0] }} </p>
+    <button class="add-to-cart">Add to Cart</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const data = await response.json();
+        this.items = data.products;
+        console.log(data)
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
+
